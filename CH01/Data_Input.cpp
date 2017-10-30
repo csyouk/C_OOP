41ecc1625485957c07ed7b0f3dad580fdfd76d49
Youk Chang SU <csyouk@hanmail.net>
2017-10-30 18:21:00 +0900

    update

---
+++ CH01/Data_Input.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void){
+
+	char name[20], comAddr[50];
+	int salary;
+
+	cout << "이름 : "; 
+	cin.getline(name, 20);
+
+	cout << "월급 : ";
+	cin >> salary;
+
+	cout << "회사주소 : ";
+	cin.getline(comAddr, 50);
+
+	cout << name << "," << salary << ", " << comAddr << endl;
+
+	return 0;
+}
