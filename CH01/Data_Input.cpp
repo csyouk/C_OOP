5c16ecdcd1d21583b062e69bb6074b1b7b582430
Youk Chang SU <csyouk@hanmail.net>
2017-10-30 19:44:19 +0900

    update

--- CH01/Data_Input.cpp
+++ CH01/Data_Input.cpp
@@ -3,22 +3,19 @@ using namespace std;
 
 int main(void){
 
-	int x = 3;
-	int &y = x;
+	char name[20], comAddr[50];
+	int salary;
 
-	//char name[20], comAddr[50];
-	//int salary;
+	cout << "이름 : "; 
+	cin.getline(name, 20);
 
-	//cout << "이름 : "; 
-	//cin.getline(name, 20);
+	cout << "월급 : ";
+	cin >> salary;
 
-	//cout << "월급 : ";
-	//cin >> salary;
+	cout << "회사주소 : ";
+	cin.getline(comAddr, 50);
 
-	//cout << "회사주소 : ";
-	//cin.getline(comAddr, 50);
-
-	//cout << name << "," << salary << ", " << comAddr << endl;
+	cout << name << "," << salary << ", " << comAddr << endl;
 
 	return 0;
 }
