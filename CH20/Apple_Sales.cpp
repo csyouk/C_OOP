99862c6e92278a4ea30a5a268521224a6c7d4ae3
Youk Chang SU <csyouk@hanmail.net>
2017-11-01 15:12:34 +0900

    update

--- CH20/Apple_Sales.cpp
+++ CH20/Apple_Sales.cpp
@@ -17,6 +17,13 @@ public:
 		cout << "판매할 사과 수량 : " << numOfApples << endl;
 		cout << "수익금 : " << myMoney << endl;
 	}
+
+	int SaleApples(int money){
+		int appleCnt = money / APPLE_PRICE;
+		numOfApples -= appleCnt; // 판매개수 - 
+		myMoney += money;				 // 수익금 증가
+		return appleCnt;
+	}
 };
 
 class AppleBuyer
@@ -34,6 +41,19 @@ public:
 		cout << "구매한 사과 수량 : " << numOfApples << endl;
 		cout << "남은 금액 : " << myMoney << endl;
 	}
+
+	void BuyApples(AppleSeller & seller, int money){
+		// 사과 구매수량 seller에서 처리한다.
+		numOfApples += seller.SaleApples(money); 
+		/*
+			객체간의 통신은 메세지 전달에 의해 전달하게 되는데,
+			이러한 메세지는 그 객체에 있는 메소드를 호출하는 것이다.
+		*/
+
+		// 돈 출금
+		myMoney -= money; 
+	}
+
 };
 
 
@@ -43,5 +63,13 @@ int main(void){
 
 	seller.SalesResult();
 	buyer.BuyerResult();
+
+	buyer.BuyApples(seller, 2000);
+
+	cout << "====================" << endl;
+
+	seller.SalesResult();
+	buyer.BuyerResult();
+
 	return 0;
 }
