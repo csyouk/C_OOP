f46d33d3a10a3d8a1f2387985adc478187cd58cf
Youk Chang SU <csyouk@hanmail.net>
2017-11-02 18:48:06 +0900

    update

--- CH_22/Student_ClassEx.cpp
+++ CH_22/Student_ClassEx.cpp
@@ -2,69 +2,87 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <typeinfo>
 using namespace std;
 
 class Student
 {
 private:
-	 string name;
-	 string id;
-	 string city;
+	string name;
+	string id;
+	string city;
 public:
-	 string GetID() const { return id; }
-	 void print();
-	 void input();
+	virtual ~Student()
+	{
+		//cout << "Student obj destroyed." << endl;
+	}
+	string GetID() const { return id; }
+	virtual void print();
+	virtual void input();
 };
 
 void Student::input()
 {
 	cout << "학번, 이름, 지역명 ?  ";
-	cin >>id>> name>> city;
+	cin >> id >> name >> city;
 }
 
 void Student::print()
 {
-	 cout << "학번: " << id <<", 이름: " << name <<", 지역: " << city ;
+	cout << "학번: " << id << ", 이름: " << name << ", 지역: " << city;
 }
 
 class University : public Student   //대학정보를 저장하는 클래스
 {
 private:
-    string major;
-	float grade ;
+	string major;
+	float grade;
 public:
-	  void print();
-	  void input();
+	~University()
+	{
+		//cout << "University obj destroyed." << endl;
+	}
+	void print();
+	void input();
 };
 
 void University::input()
 {
-	 
+	Student::input();
+	cout << "전공, 학점 ? ";
+	cin >> major >> grade;
 }
 
 void University::print()
-{    
-	 
-
+{
+	Student::print();
+	cout << ", 전공:" << major << ", 학점: " << grade << endl;
 }
 
 class PostGraduate : public Student   //대학원 정보를 저장하는 클래스
 {
 private:
-    string degree;  //학위
+	string degree;  //학위
 public:
-	  void print();
-	  void input();
+	~PostGraduate()
+	{
+		//cout << "PostGraduate obj destroyed." << endl;
+	}
+	void print();
+	void input();
 };
 
 void PostGraduate::input()
 {
-	
+	Student::input();
+	cout << "학위 입력 ";
+	cin >> degree;
 }
 
 void PostGraduate::print()
-{ 
-	 
+{
+	Student::print();
+	cout << ", 학위:" << degree << endl;
 }
 
 //**************************************
@@ -83,57 +101,78 @@ public:
 	//학생정보 입력 메서드
 	void AddStudent()
 	{
-		int Cnt, i;
+		int Cnt, i, type;
 
-	   srand(time(NULL));
-	   cout << "\n입력할 건수 ? ";
-	   cin>>Cnt;
+		srand(time(NULL));
+		cout << "\n입력할 건수 ? ";
+		cin >> Cnt;
 
-	   
-
-
-
-
-
+		for (i = 0; i < Cnt; i++)
+		{
+			type = rand() % 2;
+			if (type){
+				STU[stuNum] = new PostGraduate();
+				STU[stuNum]->input();
+			}
+			else {
+				STU[stuNum] = new University();
+				STU[stuNum]->input();
+			}
+			stuNum++;
+		}
 	}
 
 	//학생정보 출력 메서드
 	void ShowStudent()
 	{
-		cout<<"\n=============================================="<<endl;
-		
-
-
-
-		cout<<"=============================================="<<endl;
+		int i;
+		cout << "\n==============================================" << endl;
+		for (i = 0; i < stuNum; i++)
+		{
+			STU[i]->print();
+		}
+		cout << "==============================================" << endl;
 	}
 
 	//학생정보 검색 메서드
 	void StuFind()
-    {
-		 string inputID;
-		 string _end="end";
-		 int found;
-
-		 while(1) 
-		 { 
-			 cout <<"\n검색할 ID ? (종료:end) ";
-			 cin>>inputID;
-
-			 found=1;
-			 
-
-
-
+	{
+		string inputID;
+		string _end = "end";
+		int found;
 		
-		  } //while(1) end
-    }
+		while (1)
+		{
+			int i;
+			cout << "\n검색할 ID ? (종료:end) ";
+			cin >> inputID;
+
+			found = 0;
+
+			if (inputID.compare(_end) == 0){
+				return;
+			}
+
+			for (i = 0; i < stuNum; i++){
+				if (STU[i]->GetID() == inputID){
+					STU[i]->print();
+					found = 1;
+					break;
+				}
+			}
+
+			if (found){
+				cout << inputID << " Not found!!!" << endl;
+			}
+
+		} 
+	}
 
 	//동적메모리 할당해제 
 	void Stu_delete()
 	{
-		for(int i=0;i<stuNum;i++)
-		    delete STU[i]; 
+		for (int i = 0; i < stuNum; i++)
+			delete STU[i];
 	}
 };
 
@@ -142,31 +181,31 @@ public:
 int main()
 {
 	StudentHandler StuHandler;
-	int select, stop=1;
+	int select, stop = 1;
 
-	while(stop)
-	{ 
-		cout<<"\n1. 학생데이터 입력 "<<endl;
-		cout<<"2. 학생데이터 출력 "<<endl;
-		cout<<"3. 학생데이터 검색 "<<endl;
-		cout<<"4. 프로그램 종료 "<<endl;
-		cout<<"Select ? (1~4) ";
-		cin>>select;
+	while (stop)
+	{
+		cout << "\n1. 학생데이터 입력 " << endl;
+		cout << "2. 학생데이터 출력 " << endl;
+		cout << "3. 학생데이터 검색 " << endl;
+		cout << "4. 프로그램 종료 " << endl;
+		cout << "Select ? (1~4) ";
+		cin >> select;
 
-		switch(select)
+		switch (select)
 		{
 		case 1: StuHandler.AddStudent();
 			break;
-        case 2: StuHandler.ShowStudent();
+		case 2: StuHandler.ShowStudent();
 			break;
 		case 3: StuHandler.StuFind();
 			break;
 		case 4: StuHandler.Stu_delete();
-				stop=0;
-				break;
+			stop = 0;
+			break;
 		}
 	}
 
-	 return 0 ;
+	return 0;
 }
 
